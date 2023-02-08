dc58958ed10e98d42253c724d17b4513f981e964
Pranab Shukla <pranab.austin@gmail.com>
2023-02-07 21:56:11 -0600

    made auth work

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const btn = document.getElementById("keysubmit")
 const key = document.getElementById("key")
 const result = document.getElementById("result")
+const final = document.getElementById("final")
+
+final.style.display = "none";
 
 btn.addEventListener("click", () => {
     fetch("https://passwordchecker.cyclic.app", {
@@ -15,5 +18,11 @@ btn.addEventListener("click", () => {
     }).then((data) => {
         console.log(data)
         result.innerText = data.body.authenticated;
+        setTimeout(() => {
+            btn.style.display = "none";
+            key.style.display = "none";
+            result.style.display = "none";
+        }, 5000)
+        final.style.display = "block";
     })
 })
